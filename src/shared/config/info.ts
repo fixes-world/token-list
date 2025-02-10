edf75113a268a1af10dfd6720e08833d3455fa78
Tang Bo Hao <tech@btang.cn>
2025-02-10 17:45:52 +0800

    fix: revert import.meta.env

--- src/shared/config/info.ts
+++ src/shared/config/info.ts
@@ -1,4 +1,4 @@
-const network = import.meta?.env?.PUBLIC_FLOW_NETWORK ?? "testnet";
+const network = import.meta.env.PUBLIC_FLOW_NETWORK ?? "testnet";
 
 const contractAddr =
   network === "testnet" ? "0xb86f928a1fa7798e" : network === "mainnet" ? "0x15a918087ab12d86" : "";
@@ -12,19 +12,19 @@ export default {
     "TokenList is a platform for registering Fungible/Non-Fungible Tokens on Flow Blockchain. It supports permissionless on-chain registration of any Flow FT with on-chain MetadataViews and provides an API endpoint to expose the Uniswap standard token list JSON file.",
   author: "Fixes Labs",
   version: "1.0.0",
-  url: import.meta?.env?.SITE ?? "http://localhost:4321",
+  url: import.meta.env.SITE ?? "http://localhost:4321",
   staticHost: "https://static.fixes.world",
-  icon: import.meta?.env?.PUBLIC_ICON ?? "https://fixes.world/apple-touch-icon.png",
-  banner: import.meta?.env?.PUBLIC_BANNER ?? "https://i.imgur.com/Wdy3GG7.jpg",
-  linktree: import.meta?.env?.PUBLIC_LINKTREE ?? "https://linktr.ee/fixes.world",
-  twitter: import.meta?.env?.PUBLIC_TWITTER ?? "https://x.com/fixesWorld",
-  github: import.meta?.env?.PUBLIC_GITHUB ?? "https://github.com/fixes-world/token-list",
+  icon: import.meta.env.PUBLIC_ICON ?? "https://fixes.world/apple-touch-icon.png",
+  banner: import.meta.env.PUBLIC_BANNER ?? "https://i.imgur.com/Wdy3GG7.jpg",
+  linktree: import.meta.env.PUBLIC_LINKTREE ?? "https://linktr.ee/fixes.world",
+  twitter: import.meta.env.PUBLIC_TWITTER ?? "https://x.com/fixesWorld",
+  github: import.meta.env.PUBLIC_GITHUB ?? "https://github.com/fixes-world/token-list",
   documentation: "https://docs.fixes.world/concepts/token-list",
   // Blockchain info
   network: network,
   chainId: chainId,
   contractAddr: contractAddr,
   walletConnectProjectId:
-    import.meta?.env?.PUBLIC_WALLET_CONNECT_PROJECT_ID ?? "779bb5171b7c2aea7b27968a964ce083",
-  bloctoProjectId: import.meta?.env?.PUBLIC_BLOCTO_PROJECT_ID ?? "fcc27ab8-bc8a-4cb5-87ce-3b9c2cc644e9",
+    import.meta.env.PUBLIC_WALLET_CONNECT_PROJECT_ID ?? "779bb5171b7c2aea7b27968a964ce083",
+  bloctoProjectId: import.meta.env.PUBLIC_BLOCTO_PROJECT_ID ?? "fcc27ab8-bc8a-4cb5-87ce-3b9c2cc644e9",
 };
